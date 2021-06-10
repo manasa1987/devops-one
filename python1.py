aa23b7e66ca02c6ae194ac56787e8d64ad39d58e
root <root@ip-172-31-26-3.us-west-2.compute.internal>
2021-06-10 15:04:05 +0000

    ec2 start and stop modified data

--- python1.py
+++ python1.py
@@ -6,3 +6,5 @@ ec2 = boto3.client('ec2', region_name=region)
 def lambda_handler(event, context):
     ec2.stop_instances(InstanceIds=instances)
     print('stopped your instances: ' + str(instances))
+
+stopping EC2 instances
